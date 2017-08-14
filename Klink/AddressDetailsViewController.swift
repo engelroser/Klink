b0fbd198a13f1ed4eacde8dfc438ddd9703c5651
mobiel dev <engel.roser99999@gmail.com>
2017-08-14 04:57:41 +0300

    AddressDetailsViewController

---
+++ Klink/AddressDetailsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AddressDetailsViewController.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 21/10/15.
+//  Copyright © 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class AddressDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
