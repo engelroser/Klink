9b0a45d16f4f1d81847441ebfe6b77709caf858b
mobiel dev <engel.roser99999@gmail.com>
2017-08-15 19:16:07 +0300

    BaseViewController

---
+++ Klink/BaseViewController.swift
@@ -0,0 +1,39 @@
+//
+//  BaseViewController.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 24/08/15.
+//  Copyright (c) 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
