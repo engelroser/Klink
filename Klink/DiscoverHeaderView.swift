e1bbec36e7ddaf2634245db3911b316766a5885f
mobiel dev <engel.roser99999@gmail.com>
2017-08-28 04:22:58 +0300

    DiscoverHeaderView

---
+++ Klink/DiscoverHeaderView.swift
@@ -0,0 +1,30 @@
+//
+//  DiscoverHeaderView.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 23/10/15.
+//  Copyright © 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class DiscoverHeaderView: UIView {
+    
+    var maxHeight:CGFloat = 0
+
+//    override func pointInside(point: CGPoint, withEvent event: UIEvent?) -> Bool {
+//
+//        if maxHeight > 300 {
+//            if point.y > self.frame.size.height - 164 && point.y < self.frame.size.height{
+//                return true
+//            }
+//            return false
+//        } else {
+//            if point.y > self.frame.size.height - 64 && point.y < self.frame.size.height{
+//                return true
+//            }
+//            return false
+//        }
+//    }
+
+}
