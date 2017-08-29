054db44780a56677c134f7fce6d779778fcae427
mobiel dev <engel.roser99999@gmail.com>
2017-08-29 14:48:49 +0300

    FeaturedTableViewCell

---
+++ Klink/FeaturedTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeaturedTableViewCell.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 22/09/15.
+//  Copyright © 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedTableViewCell: KlinkBaseTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
