bab7c84dc7edc2dc1b04a6baae21f8994b54feb2
mobiel dev <engel.roser99999@gmail.com>
2017-09-04 04:09:56 +0300

    ItemCardFirstTableViewCell

---
+++ Klink/ItemCardFirstTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ItemCardFirstTableViewCell.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 18/09/15.
+//  Copyright (c) 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class ItemCardFirstTableViewCell: KlinkBaseTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
