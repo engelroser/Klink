a02e7641b382ee0c6c2cc5b18ca45bc316ee3d6c
mobiel dev <engel.roser99999@gmail.com>
2017-09-06 09:35:41 +0300

    KlinkBarButtonItem

---
+++ Klink/KlinkBarButtonItem.swift
@@ -0,0 +1,13 @@
+//
+//  KlinkBarButtonItem.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 24/08/15.
+//  Copyright (c) 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class KlinkBarButtonItem: UIBarButtonItem {
+   
+}
