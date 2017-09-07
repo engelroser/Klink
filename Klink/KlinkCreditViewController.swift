bfcfa4e5cd79db0a1086a7829540672ce9b7be3f
mobiel dev <engel.roser99999@gmail.com>
2017-09-07 11:22:02 +0300

    KlinkCreditViewController

---
+++ Klink/KlinkCreditViewController.swift
@@ -0,0 +1,46 @@
+//
+//  KlinkCreditViewController.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 23/09/15.
+//  Copyright © 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class KlinkCreditViewController: UIViewController {
+    
+    
+    init() {
+        super.init(nibName: "KlinkCreditViewController", bundle: nil)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        navigationItem.setKlinkTitleView("KLINK CREDIT")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
