5ac40196852311b54b7f6d9f881c2e0ab22f56bf
mobiel dev <engel.roser99999@gmail.com>
2017-09-07 11:23:26 +0300

    KlinkErrorModel

---
+++ Klink/KlinkErrorModel.swift
@@ -0,0 +1,15 @@
+//
+//  KlinkErrorModel.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 17/11/15.
+//  Copyright © 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+final class KlinkErrorModel: NSError {
+//    var error:NSError?
+    var serverData:AnyObject?
+}
