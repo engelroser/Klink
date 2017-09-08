3cc71cdaf7ca190dfc5af086b626306eb3123b58
mobiel dev <engel.roser99999@gmail.com>
2017-09-08 06:55:24 +0300

    KlinkLoading

---
+++ Klink/KlinkLoading.swift
@@ -0,0 +1,14 @@
+//
+//  KlinkLoading.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 15/10/15.
+//  Copyright © 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class KlinkLoading: NSObject {
+    // TODO: implement loading screen
+    
+}
