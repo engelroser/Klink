6714aea92eb5c1850c999fbc4837d270a23632db
mobiel dev <engel.roser99999@gmail.com>
2017-09-12 04:05:04 +0300

    KlinkTableView

---
+++ Klink/KlinkTableView.swift
@@ -0,0 +1,17 @@
+//
+//  KlinkTableView.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 03/09/15.
+//  Copyright (c) 2015 Ars Futura. All rights reserved.
+//
+
+import Foundation
+
+extension UITableView {
+    
+    
+    func setKlinkTable() {
+        
+    }
+}
