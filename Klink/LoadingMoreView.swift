9351e5927fe3dcec24feca2592c9f41eb3837f54
mobiel dev <engel.roser99999@gmail.com>
2017-09-14 04:56:45 +0300

    LoadingMoreView

---
+++ Klink/LoadingMoreView.swift
@@ -0,0 +1,29 @@
+//
+//  LoadingMoreView.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 02/11/15.
+//  Copyright © 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class LoadingMoreView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    func startLoading() {
+        
+    }
+    
+    func stopLoading() {
+        
+    }
+
+}
