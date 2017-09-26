537c6c35c8f8da418d4c7c1f05e2333365254560
mobiel dev <engel.roser99999@gmail.com>
2017-09-26 07:26:41 +0300

    PackageItemModel

---
+++ Klink/PackageItemModel.swift
@@ -0,0 +1,44 @@
+//
+//  PackageItemModel.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 03/11/15.
+//  Copyright © 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class PackageItemModel: NSObject {
+    
+    var id:Int!
+    var quantity:Int!
+    
+}
+
+//{
+//    "id": 1,
+//    "name": "Bud Light Hard Party",
+//    "description": "It is a package for hard party",
+//    "package_items": [
+//    {
+//    "id": 1,
+//    "quantity": 4,
+//    "product_variant": {
+//    "id": 5,
+//    "presentation": "BudLight 24-Pack Bottles",
+//    "options": [
+//    {
+//    "name": "Container type",
+//    "value": "Bottle"
+//    },
+//    {
+//    "name": "Pack size",
+//    "value": "24"
+//    }
+//    ],
+//    "sku": "budlight24bottles",
+//    "price": 1000,
+//    "on_hold": 0,
+//    "available_on_demand": true
+//    }
+//    },
