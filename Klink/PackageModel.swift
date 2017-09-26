eef775785df1ca8aafd8ebd827369733651a6493
mobiel dev <engel.roser99999@gmail.com>
2017-09-26 07:27:10 +0300

    PackageModel

---
+++ Klink/PackageModel.swift
@@ -0,0 +1,13 @@
+//
+//  PackageModel.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 30/10/15.
+//  Copyright © 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class PackageModel: NSObject {
+
+}
