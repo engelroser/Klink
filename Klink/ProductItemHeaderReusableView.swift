bae74112a6634ee6b4871e59699cf03f2aa204d3
mobiel dev <engel.roser99999@gmail.com>
2017-10-24 18:18:06 +0300

    ProductItemHeaderReusableView

---
+++ Klink/ProductItemHeaderReusableView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductItemHeaderReusableView.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 11/09/15.
+//  Copyright (c) 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class ProductItemHeaderReusableView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
