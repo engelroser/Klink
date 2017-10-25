42a22df0c8a44f2d134e25b6292d57cdd407652c
mobiel dev <engel.roser99999@gmail.com>
2017-10-25 15:46:13 +0300

    ProductItemScrollSubview

---
+++ Klink/ProductItemScrollSubview.swift
@@ -0,0 +1,21 @@
+//
+//  ProductItemScrollSubview.swift
+//  Klink
+//
+//  Created by Mobile App Dev on 11/09/15.
+//  Copyright (c) 2015 Ars Futura. All rights reserved.
+//
+
+import UIKit
+
+class ProductItemScrollSubview: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
